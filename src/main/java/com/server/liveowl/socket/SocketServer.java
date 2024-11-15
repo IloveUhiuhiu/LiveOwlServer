fc1aac5ea347780a2d57735865f2217aa84f2908
IloveUhiuhiu <dangphuclong2019@gmail.com>
2024-11-15 21:33:22 +0700

    long push 15/11

--- src/main/java/com/server/liveowl/socket/SocketServer.java
+++ src/main/java/com/server/liveowl/socket/SocketServer.java
@@ -5,17 +5,18 @@ import java.net.*;
 import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.Logger;
-import static com.server.liveowl.socket.SocketServer.maxDatagramPacketLength;
 
 public class SocketServer implements Runnable {
-    public static int maxDatagramPacketLength = 1500;
-    private static final int serverPort = 9000;
-    public static DatagramSocket serverSocket = null;
-    public static Map<String, ProcessGetData> teachers = new HashMap<>();
-    public static ConcurrentLinkedQueue<String> listIds = new ConcurrentLinkedQueue<>();
-    public static Map<String, byte[]> imageBuffer = new HashMap<>();
-    public static Map<String, Integer> numberBuffer = new HashMap<>();
     public static int imageCount = 0;
+    private static final int serverPort = 9000;
+    public static int maxDatagramPacketLength = 1500;
+    public static DatagramSocket serverSocket = null;
+
+    public static Map<String, byte[]> imageBuffer = new HashMap<>();
+    //public static Map<String, Integer> numberBuffer = new HashMap<>();
+    public static Map<String, ProcessGetData> listMeeting = new HashMap<>();
+    public static ConcurrentLinkedQueue<String> listIds = new ConcurrentLinkedQueue<>();
+
     private final static Logger audit = Logger.getLogger("requests");
     private final static Logger errors = Logger.getLogger("errors");
     public void run() {
@@ -35,17 +36,16 @@ public class SocketServer implements Runnable {
                 if (role.equals("student")) {
                     String code = UdpHandler.receiveMsg(serverSocket,address,port);
                     System.out.println("Student gửi mã: " + code);
-                    if (!SocketServer.teachers.containsKey(code)) {
+                    if (!SocketServer.listMeeting.containsKey(code)) {
                         System.out.println("Student gửi mã không có trong map");
                         UdpHandler.sendMsg(serverSocket,"fail",address,port);
                         continue;
                     } else {
                         UdpHandler.sendMsg(serverSocket,"success",address,port);
                     }
-
-                    SocketServer.teachers.get(code).addStudent(countStudents, packet);
+                    SocketServer.listMeeting.get(code).addStudent(countStudents, packet);
                     UdpHandler.sendNumber(serverSocket,countStudents,address,port);
-                    UdpHandler.sendNumber(serverSocket,SocketServer.teachers.get(code).numberOfProcess,address,port);
+                    UdpHandler.sendNumber(serverSocket,SocketServer.listMeeting.get(code).numberOfProcess,address,port);
                     ++countStudents;
                     System.out.println("Trả về số cổng thành công");
                 } else if (role.equals("teacher")) {
@@ -56,231 +56,19 @@ public class SocketServer implements Runnable {
                     ++countConnect;
                     DatagramSocket clientSocket = new DatagramSocket(serverPort + countConnect);
                     DatagramSocket clientSocket2 = new DatagramSocket(serverPort + 50 + countConnect);
-                    ProcessGetData theTeacher = new ProcessGetData(clientSocket,clientSocket2,packet,code,countConnect);
-                    ProcessSendData theTecher = new ProcessSendData(packet.getAddress(), packet.getPort());
+                    ProcessGetData processGetData = new ProcessGetData(clientSocket,clientSocket2,packet,code,countConnect);
+                    ProcessSendData processSendData = new ProcessSendData(packet);
                     UdpHandler.sendNumber(serverSocket,countConnect,address,port);
                     System.out.println("Trả về số cổng thành công");
-                    SocketServer.teachers.put(code, theTeacher);
-                    new Thread(theTeacher).start();
-                    new Thread(theTecher).start();
+                    SocketServer.listMeeting.put(code, processGetData);
+                    new Thread(processGetData).start();
+                    new Thread(processSendData).start();
                 } else {
 
                 }
             }
         } catch (IOException e) {
             System.err.println("Lỗi server: " + e.getMessage());
-        }
-    }
-}
-
-class ProcessGetData implements Runnable {
-    public static int numberOfProcess;
-    private final DatagramSocket theSocket;
-    private final DatagramSocket theSocket2;
-    public InetAddress addressTeacher;
-    public int portTeacher;
-    private final String code;
-    Map<Integer, Integer> portStudents = new HashMap<>();
-    Map<Integer, InetAddress> addressStudents = new HashMap<>();
-    Map<Integer, Boolean> disconnect = new HashMap<>();
-
-    public ProcessGetData(DatagramSocket theSocket,DatagramSocket theSocket2,DatagramPacket thePacket, String code, int numberOfProcess) throws IOException {
-        this.theSocket = theSocket;
-        this.theSocket2 = theSocket2;
-        this.portTeacher = thePacket.getPort();
-        this.addressTeacher = thePacket.getAddress();
-        this.code = code;
-        this.numberOfProcess = numberOfProcess;
-    }
-    public void addStudent(int numerOfStudent, DatagramPacket thPacket) {
-        portStudents.put(numerOfStudent, thPacket.getPort());
-        addressStudents.put(numerOfStudent, thPacket.getAddress());
-    }
-
-    public void run() {
-
-        try {
-            while(true) {
-                byte[] message = new byte[maxDatagramPacketLength];
-                UdpHandler.receiveBytesArr(theSocket,message);
-                int packetType = (message[0] & 0xff);
-                if (packetType == 0) {
-                    int clientId = (message[1] & 0xff);
-                    if (disconnect.containsKey(clientId)) {
-                        continue;
-                    }
-                    int imageId = (message[2] & 0xff);
-                    int lengthOfImage = (message[3] & 0xff) << 16 | (message[4] & 0xff) << 8 | (message[5] & 0xff);
-                    int numberOfPacket = message[6] & 0xff;
-                    byte[] imageBytes = new byte[lengthOfImage];
-                    String Key = imageId + ":" + clientId;
-                    SocketServer.imageBuffer.put(Key, imageBytes);
-                    SocketServer.numberBuffer.put(Key,numberOfPacket);
-
-                } else if (packetType == 1){
-                    int clientId = (message[1] & 0xff);
-                    if (disconnect.containsKey(clientId)) {
-                        continue;
-                    }
-                    int packetId = (message[2] & 0xff);
-                    int sequenceNumber = (message[3] & 0xff);
-                    boolean isLastPacket = ((message[4] & 0xff) == 1);
-                    int destinationIndex = (sequenceNumber - 1) * (maxDatagramPacketLength - 5);
-                    String Key = packetId + ":" + clientId;
-                    if (SocketServer.imageBuffer.containsKey(Key)) {
-                        int lengthOfImage = SocketServer.imageBuffer.get(Key).length;
-                        byte[] imageBytes = SocketServer.imageBuffer.get(Key);
-                        SocketServer.numberBuffer.put(Key, SocketServer.numberBuffer.get(Key) - 1) ;
-                        if (destinationIndex >= 0 && destinationIndex < lengthOfImage) {
-                            if (!isLastPacket && (destinationIndex + (maxDatagramPacketLength - 5) < lengthOfImage)) {
-                                System.arraycopy(message, 5, imageBytes, destinationIndex, maxDatagramPacketLength - 5);
-                            } else {
-                                System.arraycopy(message, 5, imageBytes, destinationIndex, lengthOfImage % (maxDatagramPacketLength - 5));
-                            }
-                            if (SocketServer.numberBuffer.get(Key) == 0) {
-                                SocketServer.listIds.add(Key);
-                            }
-                        } else {
-                            System.out.println("Chỉ số đích không hợp lệ: " + destinationIndex + ", lengthOfimage" + lengthOfImage);
-                        }
-                    } else {
-                        System.out.println("Lỗi ID_Paket bị xóa khỏi buffer!");
-                    }
-                } else if (packetType == 2) {
-                    try {
-                        int clientId = (message[1] & 0xff);
-                        int port = portStudents.get(clientId) - 1000;
-                        InetAddress address = addressStudents.get(clientId);
-                        UdpHandler.sendRequests(theSocket2, "camera".getBytes(), address, port);
-                    } catch (IOException e) {
-                        System.out.println(e.getMessage());
-                    }
-                } else if (packetType == 3) {
-                    System.out.println("send exit to student");
-                    for (int key : portStudents.keySet()) {
-                        System.out.println(addressStudents.get(key) + ", " + portStudents.get(key));
-                        UdpHandler.sendRequests(theSocket2,"exit".getBytes(), addressStudents.get(key), portStudents.get(key) - 1000);
-                    }
-                    try {
-                        if (theSocket != null && !theSocket.isClosed()) {
-                            theSocket.close();
-                        }
-                        if (theSocket2 != null && !theSocket2.isClosed()) {
-                            theSocket2.close();
-                        }
-                    } catch (Exception ex) {
-                        System.out.println("Lỗi khi đóng socket: " + ex.getMessage());
-                    }
-                    SocketServer.imageBuffer.clear();
-                    portStudents.clear();
-                    addressStudents.clear();
-                    SocketServer.teachers.remove(code);
-                } else if (packetType == 4) {
-                    System.out.println("send exit to teacher");
-                    int clientId = (message[1] & 0xff);
-                    byte[] numberBytes = new byte[2];
-                    numberBytes[0] = (byte) (4);
-                    numberBytes[1] = (byte) (clientId);
-                    System.out.println("ID học sinh" + clientId);
-                    disconnect.put(clientId,true);
-                    portStudents.remove(clientId);
-                    addressStudents.remove(clientId);
-                    System.out.println("remove address và port thành công");
-                    Iterator<String> iterator = SocketServer.imageBuffer.keySet().iterator();
-                    while (iterator.hasNext()) {
-                        String key = iterator.next();
-                        String ID = key.substring(key.indexOf(":") + 1);
-                        if (Integer.parseInt(ID) == clientId) {
-                            iterator.remove();
-                        }
-                    }
-                    System.out.println("remove từ buffer");
-                    UdpHandler.sendRequests(theSocket2,numberBytes,addressTeacher, portTeacher);
-                }
-            }
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        } finally {
-            try {
-                if (theSocket != null ) theSocket.close();
-                if (theSocket2 != null) theSocket2.close();
-            } catch (Exception e) {
-
-            }
-        }
-    }
-//    public synchronized void sendRequestExitForStudent(byte[] imageByteArray,InetAddress address, int port) throws IOException {
-//        DatagramPacket packet = new DatagramPacket(imageByteArray, imageByteArray.length, address, port);
-//        theSocket2.send(packet);
-//    }
-//    public synchronized void sendRequestExitForTeacher(byte[] imageByteArray,InetAddress address, int port) throws IOException {
-//        DatagramPacket packet = new DatagramPacket(imageByteArray, imageByteArray.length, address, port);
-//        theSocket2.send(packet);
-//    }
-//    public synchronized void sendRequestCameraForStudent(byte[] imageByteArray,InetAddress address,int port) throws IOException {
-//        DatagramPacket packet = new DatagramPacket(imageByteArray, imageByteArray.length, address, port);
-//        theSocket2.send(packet);
-//    }
-}
-class ProcessSendData implements Runnable {
-    DatagramSocket socket;
-    public InetAddress addressTeacher;
-    public int portTeacher;
-    ProcessSendData(InetAddress addr, int port) throws SocketException {
-        socket = new DatagramSocket(9080);
-        addressTeacher = addr;
-        portTeacher = port;
-    }
-    @Override
-    public void run() {
-        while(true) {
-            try {
-                String packetId = SocketServer.listIds.poll();
-                if (packetId != null) {
-                    byte[] imageByteArray = SocketServer.imageBuffer.get(packetId);
-                    int pos = packetId.lastIndexOf(":");
-                    int imageId = Integer.parseInt(packetId.substring(0, pos));
-                    int clientId = Integer.parseInt(packetId.substring(pos + 1));
-                    int sequenceNumber = 0;
-                    boolean flag;
-                    int length = imageByteArray.length;
-                    byte[] lengthBytes = new byte[maxDatagramPacketLength];
-                    lengthBytes[0] = (byte) 0;
-                    lengthBytes[1] = (byte) (clientId);
-                    lengthBytes[2] = (byte) (imageId);
-                    lengthBytes[3] = (byte) (length >> 16);
-                    lengthBytes[4] = (byte) (length >> 8);
-                    lengthBytes[5] = (byte) (length);
-                    lengthBytes[6] = (byte) ((length + maxDatagramPacketLength - 6) / (maxDatagramPacketLength - 5));
-                    UdpHandler.sendBytesArray(socket, lengthBytes, addressTeacher, portTeacher);
-                    for (int i = 0; i < length; i = i + maxDatagramPacketLength - 5) {
-                        sequenceNumber += 1;
-                        byte[] message = new byte[maxDatagramPacketLength];
-                        message[0] = (byte) (1);
-                        message[1] = (byte) (clientId);
-                        message[2] = (byte) (imageId);
-                        message[3] = (byte) (sequenceNumber);
-                        if ((i + maxDatagramPacketLength - 5) >= imageByteArray.length) {
-                            flag = true;
-                            message[4] = (byte) (1);
-                        } else {
-                            flag = false;
-                            message[4] = (byte) (0);
-                        }
-
-                        if (!flag) {
-                            System.arraycopy(imageByteArray, i, message, 5, maxDatagramPacketLength - 5);
-                        } else {
-                            System.arraycopy(imageByteArray, i, message, 5, length - i);
-                        }
-                        UdpHandler.sendBytesArray(socket, message, addressTeacher, portTeacher);
-                    }
-                    ++SocketServer.imageCount;
-                    System.out.println("Gửi thành công ảnh thứ: " + SocketServer.imageCount + "length = " + length);
-                }
-            } catch (Exception e) {
-                System.out.println("Loi khi gui anh:" + e.getMessage());
-            }
         }
     }
 }
