83a81946d6bd836c36bdfe39fc6a170658a21da3
IloveUhiuhiu <dangphuclong2019@gmail.com>
2024-12-28 16:26:12 +0700

    chang code 28/12

--- src/main/java/com/server/liveowl/LiveowlApplication.java
+++ src/main/java/com/server/liveowl/LiveowlApplication.java
@@ -1,14 +1,14 @@
 package com.server.liveowl;
 
-
 import com.server.liveowl.keylogger.FileServer;
 import com.server.liveowl.savevideo.VideoServer;
 import com.server.liveowl.socket.SocketServer;
 import org.opencv.core.Core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import static com.server.liveowl.ServerConfig.sendkeyboardPath;
-import static com.server.liveowl.ServerConfig.SEND_KEYBOARD_PORT;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 
 @SpringBootApplication
@@ -19,9 +19,11 @@ public class LiveowlApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(LiveowlApplication.class, args);
-		new Thread(new VideoServer()).start();
-		new Thread(new SocketServer()).start();
-		new Thread(new FileServer()).start();
+		ExecutorService executor = Executors.newFixedThreadPool(3);
+		executor.submit(new VideoServer());
+		executor.submit(new SocketServer());
+		executor.submit(new FileServer());
+		executor.shutdown();
 
 	}
 
--- src/main/java/com/server/liveowl/socket/SocketServer.java
+++ src/main/java/com/server/liveowl/socket/SocketServer.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.util.logging.Logger;
 import java.util.concurrent.Executors;
 
+import org.opencv.videoio.VideoWriter;
 import java.util.concurrent.ExecutorService;
 import com.server.liveowl.util.FileHandler;
 import com.server.liveowl.util.UdpHandler;
@@ -11,18 +12,16 @@ import static com.server.liveowl.ServerConfig.*;
 
 public class SocketServer implements Runnable {
 
-
     private static int numberOfConnect = 0;
-    public static DatagramSocket serverSocket = null;
     public static Map<String, ProcessGetImage> listMeeting = new HashMap<>();
     private final static Logger audit = Logger.getLogger("requests");
     private final static Logger errors = Logger.getLogger("errors");
     private ExecutorService executor = Executors.newFixedThreadPool(NUM_OF_THREAD);
+
     public void run() {
-        try {
-            serverSocket = new DatagramSocket(SERVER_PORT);
+        try (DatagramSocket serverSocket = new DatagramSocket(SERVER_PORT)){
+            System.out.println("Server đang lắng nghe ...");
             while (true) {
-                System.out.println("Server đang lắng nghe ...");
                 DatagramPacket packet = UdpHandler.getPacket(serverSocket);
                 String connect = new String(packet.getData(), 0, packet.getLength());
                 executor.execute(() -> handleClient(packet, serverSocket, connect));
@@ -30,9 +29,8 @@ public class SocketServer implements Runnable {
         } catch (Exception e) {
             System.err.println("Lỗi server: " + e.getMessage());
         } finally {
-            if (serverSocket != null) serverSocket.close();
-            listMeeting.clear();
             executor.shutdown();
+            listMeeting.clear();
         }
     }
 
@@ -42,8 +40,10 @@ public class SocketServer implements Runnable {
             String clientId = connect.split(":")[0];
             String role = connect.split(":")[1];
             String code = connect.split(":")[2];
+
             InetAddress address = packet.getAddress();
             int port = packet.getPort();
+
             if (role.equals("student")) {
                 System.out.println("Student gửi mã: " + code);
                 if (!SocketServer.listMeeting.containsKey(code)) {
@@ -52,27 +52,28 @@ public class SocketServer implements Runnable {
                 } else {
                     UdpHandler.sendMsg(serverSocket,"success",address,port);
                 }
-                ProcessGetImage processGetData = listMeeting.get(code);
-                processGetData.addStudent(clientId, packet);
-                UdpHandler.sendNumber(serverSocket,processGetData.processId,address,port);
+                ProcessGetImage processGetImage = listMeeting.get(code);
+                processGetImage.addStudent(clientId, packet);
+                UdpHandler.sendNumber(serverSocket,processGetImage.getProcessId(),address,port);
 
             } else if (role.equals("teacher")) {
                 ++numberOfConnect;
-                ProcessGetImage processGetData = new ProcessGetImage(packet,code,clientId,numberOfConnect);
-                ProcessSendImage processSendData = new ProcessSendImage(processGetData);
-                ProcessSaveImage processSavedData = new ProcessSaveImage(processGetData);
-                ProcessGetKey serverKeylogger = new ProcessGetKey(processGetData);
+                ProcessGetImage processGetImage = new ProcessGetImage(packet,code,clientId,numberOfConnect);
+                ProcessSendImage processSendImage = new ProcessSendImage(processGetImage);
+                ProcessSaveImage processSaveImage = new ProcessSaveImage(processGetImage);
+                ProcessGetKey processGetKey = new ProcessGetKey(processGetImage);
 
                 UdpHandler.sendNumber(serverSocket,numberOfConnect,address,port);
                 System.out.println("Trả về số cổng thành công");
-                SocketServer.listMeeting.put(code, processGetData);
-                FileHandler.checkAndCreateFolder(videoPath + "\\_" +code);
-                FileHandler.checkAndCreateFolder(keyboardPath + "\\_" + code);
+                listMeeting.put(code, processGetImage);
 
-                new Thread(processGetData).start();
-                new Thread(processSendData).start();
-                new Thread(processSavedData).start();
-                new Thread(serverKeylogger).start();
+                FileHandler.checkAndCreateFolder(VIDEO_PATH + "\\_" +code);
+                FileHandler.checkAndCreateFolder(KEYBOARD_PATH + "\\_" + code);
+
+                new Thread(processGetImage).start();
+                new Thread(processSendImage).start();
+                new Thread(processSaveImage).start();
+                new Thread(processGetKey).start();
             }
 
         } catch (Exception e) {
